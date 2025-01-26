b5749108d6193c2c446c52d6c8972ca4288867f4
Maximiliano <juanmaximilianoprieto@hotmail.com>
2025-01-26 17:08:07 +0100

    hacemos un reloj con bom

--- JS/10-Bom/app.js
+++ JS/10-Bom/app.js
@@ -3,15 +3,29 @@
 //alert("mensaje novo");   alert y prompt son parte de BOM
 
 const comenzar = () => {
-    console.log("primer mensaje");
+  console.log("primer mensaje");
 };
 
 setTimeout(comenzar, 5000); // cuando pasan 5000ms se ejecuta la función
 
 setTimeout(() => {
-    console.log("hola homero")
-}, 1000);// tambien podemos poner la funcion adentro directamente 
+  console.log("hola homero");
+}, 1000); // tambien podemos poner la funcion adentro directamente
 
-setInterval(() => {
-    console.log("chinchecnji")//setinterval repite la funcion cada el tiempo que se le indique 
+const intervalo = setInterval(() => {
+  // Podemos agergar los intervalos a un const
+  console.log("chinchecnji"); //setinterval repite la funcion cada el tiempo que se le indique
 }, 1000);
+
+clearInterval(intervalo); // Sirve para detener el intervalo
+
+
+
+const hora = document.getElementById("Hora");
+
+const actHora=()=>{
+    const fechaactual = new Date();
+    hora.innerHTML=`${fechaactual.getHours()}:${fechaactual.getMinutes()}:${fechaactual.getSeconds()}`
+}
+
+setInterval(actHora,1000);
